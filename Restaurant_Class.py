75a7ecaf8ca100576c6b16863a43f27111162a0b
fz0053 <fernandozavala-ortiz@my.unt.edu>
2021-05-12 03:17:16 -0500

    New File

---
+++ Restaurant_Class.py
@@ -0,0 +1,18 @@
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+    
+    def describe_restaurant(self):
+        print(f"The restaurant's name is {self.restaurant_name}")
+        print(f"This restaurant serves mainly {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open")
+
+restaurant = Restaurant('Salerno\'s', 'Italian')
+
+restaurant.describe_restaurant()
+
+restaurant.open_restaurant()
